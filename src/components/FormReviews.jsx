75f40c67360e1a0b6ec1dd24b25b3fc35cdc8db9
GiuliaMoiraghi10 <gmoiraghi10@gmail.com>
2025-01-13 16:21:57 +0100

    Aggiunta style css al form

--- src/components/FormReviews.jsx
+++ src/components/FormReviews.jsx
@@ -1,23 +1,24 @@
+import style from "./FormReviews.module.css"
+
 export default function FormReviews() {
     return (<>
         <div>
-            <div>
+            <div className={style.title_form}>
                 <strong>Aggiungi recensione</strong>
             </div>
         </div>
-        <div>
+        <div className={style.form}>
             <form action="">
-                <div>
+                <div className={style.nome}>
                     <label htmlFor="nome">Nome</label>
-                    <input type="text" placeholder='Anonimo' name='name' id='name' />
+                    <input type="text" placeholder='Anonimo' name='name' id='name' className={style.input_nome} />
                 </div>
                 <div>
-                    <label htmlFor="review">Recensione</label>
-                    <textarea name="review" id="review" rows="5" placeholder='Scrivi la tua recensione...'></textarea>
+                    <textarea name="review" id="review" rows="5" placeholder='Scrivi la tua recensione...' className={style.input_text}></textarea>
                 </div>
                 <div>
                     <label htmlFor="voto">Voto</label>
-                    <select name="vote" id="vote">
+                    <select name="vote" id="vote" className={style.input_vote}>
                         <option value="1">1</option>
                         <option value="2">2</option>
                         <option value="3">3</option>
@@ -25,7 +26,7 @@ export default function FormReviews() {
                         <option value="5">5</option>
                     </select>
                 </div>
-                <button>Invia</button>
+                <button className={style.btn}>Invia</button>
             </form>
         </div>
     </>
