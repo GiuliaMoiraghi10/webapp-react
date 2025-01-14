98e46c84a994358f95a0cc9f26eb3a2160da1887
GiuliaMoiraghi10 <gmoiraghi10@gmail.com>
2025-01-14 15:57:26 +0100

    Aggiunta Link su logo

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,10 +1,11 @@
 import Navbar from "../components/Navbar"
 import style from './Header.module.css'
+import { Link } from "react-router-dom"
 
 export default function Header() {
     return (
         <div className={style.header}>
-            <h3 className={style.title}>Movies</h3>
+            <h3 className={style.title}><Link to='/'>Movies</Link></h3>
             <Navbar />
         </div>
     )
